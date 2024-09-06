de17434cd5a365840494abeac6d0ec8fa92117d2
Nathaly0TRajano <nathydelima00@gmail.com>
2024-09-06 15:16:31 -0300

    first commit

---
+++ app/Models/Tarefa.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tarefa extends Model
+// Herdar funções da classe model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'titulo', 'descricao', 'status', 'data_inicio', 'data_fim'
+    ];
+}
---
+++ app/Service/TarefaService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Tarefa;
+
+class TarefaService
+{
+    public function create(array $tarefa)
+    {
+        $dados = Tarefa::create([
+            'titulo' => $tarefa['titulo'],
+            'descricao' => $tarefa['descricao'],
+            'status' => 'Em Aberto'
+        ]);
+
+        return $dados;
+    }
+}
